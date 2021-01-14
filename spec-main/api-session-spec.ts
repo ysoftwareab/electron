7b3a70e8f4f4075a07438c277cb1c07b40374ba6
John Kleinschmidt <jkleinsc@electronjs.org>
2021-01-14 14:14:38 -0500

    try this for debugging

--- spec-main/api-session-spec.ts
+++ spec-main/api-session-spec.ts
@@ -558,12 +558,8 @@ describe('session module', () => {
       const ses = session.fromPartition(`${Math.random()}`);
       ses.setCertificateVerifyProc(({ verificationResult, errorCode }, callback) => {
         console.log('verificationResult:', verificationResult);
-        if (process.platform !== 'darwin' || process.arch !== 'arm64') {
-          expect(['net::ERR_CERT_AUTHORITY_INVALID', 'net::ERR_CERT_COMMON_NAME_INVALID'].includes(verificationResult)).to.be.true();
-        } else {
-          // TODO (jkleinsc) macos arm64 returns CERT_STATUS_INVALID or :ERR_FAILED - remove condition once this is fixed upstream
-          expect(['net::CERT_STATUS_INVALID', 'net::ERR_FAILED'].includes(verificationResult)).to.be.true();
-        }
+        console.log('errorCode:', errorCode);
+        expect(['net::ERR_CERT_AUTHORITY_INVALID', 'net::ERR_CERT_COMMON_NAME_INVALID'].includes(verificationResult)).to.be.true();
         expect([-202, -200].includes(errorCode)).to.be.true();
         callback(0);
       });
